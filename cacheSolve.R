affec3b163751fe04bb7c728d746e547ccff7335
Gregory <gpw797@gmail.com>
2014-07-14 09:57:58 -0700

    second commit

---
+++ cacheSolve.R
@@ -0,0 +1,11 @@
+cacheSolve <- function(x, ...) {
+      m <- x$getinverse()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(x)(data, ...)
+      x$setinverse(m)
+      m
+}
