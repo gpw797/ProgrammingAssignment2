e1fd6e490bac6e26595010afef55e1f154998e14
Gregory <gpw797@gmail.com>
2014-07-25 09:21:18 -0700

    FINAL SUBMIT for grading

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,11 +1,14 @@
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-      m <- x$getinverse()
+      m <- x$getinverse()                       #function returns an inverse matrix of x
       if(!is.null(m)) {
-            message("getting cached data")
+            message("getting cached data")      #only calculate the inverse if it previously set print message if is T
             return(m)
       }
-      data <- x$get()
-      m <- solve(x)(data, ...)
-      x$setinverse(m)
-      m
+      data <- x$get()                           #get the matrix
+      m <- solve(data)                          #calculate the inverse of the matrix
+      x$setinverse(m)                           #set inverse
+      m                                         #function returns the matrix
 }
--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,13 +1,20 @@
-makeCacheMatrix <- function(x = numeric()) {
-      m <- NULL
-      set <- function(y) {
-            x <<- y
-            m <<- NULL
+## This function creates a special "matrix" object that can cache its inverse
+makeCacheMatrix <- function(x = matrix()) {
+      i <- NULL                                 #initialize the inverse
+      set <- function(y) {                #method to set the matrix
+            m <<- matrix
+            i <<- NULL
       }
-      get <- function() x
-      setinverse <- function(solve(x)) m <<- solve(x)
-      getinverse <- function() m
-      list(set = set, get = get,
+      get <- function(){                  #define get function returns the matrix
+            m
+      }                         
+      setinverse <- function(inverse){          #define setinverse
+            i <<- inverse      
+      }
+      getinverse <- function(){                 #returns i
+            i
+      }
+      list(set = set, get = get,                #list is a list of the methods used
            setinverse = setinverse,
            getinverse = getinverse)
 }
